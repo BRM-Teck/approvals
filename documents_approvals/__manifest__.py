230cad9e8c69eff9d1b21b578e70eff0fa7380b5
Jessy Pango <kvnjessy@gmail.com>
2026-06-30 10:22:59 +0100

    [INIT] documents_approvals module

---
+++ documents_approvals/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Validations - Documents',
+    'version': '19.0.1.0.0',
+    'category': 'Validations',
+    'summary': "Intégration entre les validations et la gestion électronique de documents (GED).",
+    'description': """
+Validations - Documents
+=======================
+Ce module permet l'intégration entre le module de validations (approvals) et l'application Documents.
+    """,
+    'author': 'BRM-Teck',
+    'website': 'https://github.com/BRM-Teck/approvals',
+    'depends': ['approvals', 'documents'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/approval_request_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
