147fe8cbb01d4b701102f2585ca4ba1897048ea9
Jessy Pango <kvnjessy@gmail.com>
2026-07-02 09:38:29 +0100

    [IMP] approvals_purchase: version bump 19.0.1.0.1

--- approvals_purchase/__manifest__.py
+++ approvals_purchase/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Validations - Achats',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Validations',
     'summary': "Ce module ajoute au flux de validation la possibilité de générer une demande de prix à partir d'une demande de validation d'achat.",
     'description': """
