7d99b3f50ebad76f36be4e973c07d82b90a46156
Jessy Pango <kvnjessy@gmail.com>
2026-07-03 12:46:28 +0100

    [IMP] approvals: add i18n translation and bump versions

--- approvals/__manifest__.py
+++ approvals/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Approvals',
-    'version': '19.0.1.1.0',
+    'version': '19.0.1.1.1',
     'category': 'Human Resources/Approvals',
     'summary': 'Create and validate approvals',
     'description': """
